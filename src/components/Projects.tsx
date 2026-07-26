991b57f1cdfcd170791070eb1d8ce6a261f3271e
Canada Badiane <canada1555@hotmail.com>
2026-07-26 15:02:51 -0400

    feat(chore): corriger une faute d’orthographe et mise à jour du CV

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -36,7 +36,7 @@ const Projects = () => {
       languages: [
         "WordPress",
         "WooCommerce",
-        "ACF PRO",
+        "ACF Pro",
         "Polylang",
         "PHP",
         "PhpMyAdmin",
